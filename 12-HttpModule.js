6452e12b9b8559a8165c735b5f74d84b09089e47
Ahmed Hisham <ahmedhisham1928@gmail.com>
2025-01-30 12:21:51 +0200

    ..

---
+++ 12-HttpModule.js
@@ -0,0 +1,18 @@
+const http = require("http");
+
+//req = incoming request
+//res = outgoing response
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Welcome to our home page");
+  } else if (req.url === "/about") {
+    res.end("Here is our short history");
+  } else {
+    res.end(`
+        <h1>Oops!</h1>
+        <p>We can't seem to find the page you're looking for</p>
+        <a href="/">Back to home</a>
+    `);
+  }
+});
+server.listen(8080);
